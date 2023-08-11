a12aba1add03ee29c774f1bacb494532d0dde943
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-10 19:41:56 -0600

    Update SiteSwitcher.tsx

--- components/SiteSwitcher.tsx
+++ components/SiteSwitcher.tsx
@@ -54,8 +54,8 @@ function SiteSwitcher() {
           { "hover:text-black dark:hover:text-white": site }
         )}
       >
-        <SiteSwitcherLink href="/repo" text="Repo" isActive={site === "repo"} />
-        <SiteSwitcherLink href="/pack" text="Pack" isActive={site === "pack"} />
+        <SiteSwitcherLink href="/repo" text="AI" isActive={site === "repo"} />
+        <SiteSwitcherLink href="/pack" text="LAB" isActive={site === "pack"} />
       </span>
     </div>
   );
