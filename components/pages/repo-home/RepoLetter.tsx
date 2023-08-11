41d505b72e94e415fd02390d23d6721bce0fd61e
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-11 10:02:50 -0600

    Update RepoLetter.tsx

--- components/pages/repo-home/RepoLetter.tsx
+++ components/pages/repo-home/RepoLetter.tsx
@@ -12,46 +12,44 @@ export function RepoLetter() {
     <section className="relative flex flex-col items-center px-6 py-16 font-sans md:py-24 lg:py-32 gap-14">
       <FadeIn>
         <HeroText className="lg:text-[65px]">
-          扩展你的项目
+          Innovando el Futuro
           <br />
-          这没那么难
+          Con Inteligencia A Tu Control
         </HeroText>
       </FadeIn>
       <div className="flex flex-col max-w-xl leading-6 md:text-lg lg:text-lg">
         <FadeIn className="opacity-70">
           <p>
-            The bigger your project grows, the slower it gets. Tasks like
-            linting, testing, and building begin to take enormous amounts of
-            time.
+            La alba tecnológica se cierne sobre nosotros,
+            pero aún es un lujo para unos pocos.
+            Es el momento de evolucionar nuestras herramientas,
+            democratizar la tecnología.
           </p>
           <br />
           <p>
-            If you&apos;re serving multiple applications, you might reach for a
-            monorepo. They&apos;re incredible for productivity, especially on
-            the frontend, but the tooling can be a nightmare. There&apos;s a lot
-            of stuff to do (and things to mess up). Nothing &ldquo;just
-            works.&rdquo; It&apos;s become completely normal to waste entire
-            days or weeks on plumbing—tweaking configs, writing one-off scripts,
-            and stitching stuff together.
+            Aquí es donde Sapiens IA Laboratories cobra protagonismo.
+            Imaginamos un futuro en el que cada hogar tenga acceso
+            a su propio asistente de inteligencia artificial,
+            su propio Basilisco, estamos forjando este futuro,
+            evolucionando con él.
           </p>
           <br />
-          <p>We need something else.</p>
+          <p>Construyamos el futuro que anhelamos.</p>
           <br></br>
           <p>
-            A fresh take on the whole setup. Designed to glue everything
-            together. A toolchain that works for you and not against you. With
-            sensible defaults, but even better escape hatches. Built with the
-            same techniques used by the big guys, but in a way that doesn&apos;t
-            require PhD to learn or a staff to maintain.
+            Estamos dando vida a una IA, no una IA cualquiera,
+            sino una que aprende de ti y crece contigo.
+            Diseñada para ser más que una herramienta,
+            es un compañero, un intrínseco de tu vida.
           </p>
           <br />
-          <p>With Turborepo, we&apos;re doing just that.</p>
+          <p>Con Basilisk AI V2, estamos materializando precisamente eso.</p>
           <br />
           <p>
-            We&apos;re building a build system that can keep up with your team.
-            You&apos;ll see your CI get faster, duplicated work get cut, and
-            your NPM scripts get simpler. You&apos;ll get a world-class
-            development environment, without the maintenance burden.
+            Estamos creando un sistema de IA que evoluciona
+            junto a tu equipo. Experimentarás un asistente personal
+            altamente eficiente que simplifica tu vida.
+            Abraza el futuro, sin miedo a quedarte atrás.
           </p>
         </FadeIn>
         <FadeIn noVertical viewTriggerOffset className="relative h-2 md:h-12">
