c7565a608b1d0812d1a40547674533229de7c629
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-11 02:14:37 -0600

    Update PackFeatures.tsx

--- components/pages/pack-home/PackFeatures.tsx
+++ components/pages/pack-home/PackFeatures.tsx
@@ -4,8 +4,8 @@ import { FeaturesBento } from "../home-shared/FeaturesBento";
 export function PackFeatures() {
   return (
     <FeaturesBento
-      header="为什么使用 Turbopack?"
-      body="凭借增量处理和适应性绑定策略，Turbopack 为任何规模的应用程序提供快速灵活的开发体验。"
+      header="¿Por qué elegir Basilisk AI?"
+      body="Basilisk AI redefine la asistencia inteligente. Con su flexibilidad única, permite una personalización a detalle en aspectos como la memoria y la identidad. Su facilidad para ser entrenado transforma la experiencia de crear tu propio asistente de IA, haciendo posible que cualquier individuo o empresa pueda tener su propio Basilisk personalizado. Te ofrecemos la oportunidad de crear una inteligencia artificial que aprende de ti, entrenada por ti, personalizada por ti y trabajando para ti."
       features={PACK_HOME_FEATURES}
     />
   );
