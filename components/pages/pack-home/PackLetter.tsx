b85f6bf01380e5ba0c90a3e370dc6e66ddb74895
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-22 06:11:40 -0600

    Update PackLetter.tsx

--- components/pages/pack-home/PackLetter.tsx
+++ components/pages/pack-home/PackLetter.tsx
@@ -99,7 +99,7 @@ export function PackLetter() {
       <FadeIn noVertical className="relative flex justify-center w-full mt-16">
         <div className="max-w-[180px] w-full">
           <CTAButton>
-            <Link href="./chat">
+            <Link href="https://new.sapienslaboratories.com">
               <a className="block py-3 font-sans">Comienza a Construir</a>
             </Link>
           </CTAButton>
