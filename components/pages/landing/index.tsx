62810cae4654e452f432d4182833f5a0b64a8c5d
Vortextech01 <127008855+Vortextech01@users.noreply.github.com>
2023-08-22 02:57:49 -0600

    Update index.tsx

--- components/pages/landing/index.tsx
+++ components/pages/landing/index.tsx
@@ -164,12 +164,12 @@ function LandingPage() {
           </p>
           <div className="video-container">
             <iframe
-              width="560"
-              height="315"
+              width="290"
+              height="200"
               src="https://www.youtube.com/embed/Ur2dDMMH8Kc"
               title="YouTube video player"
               frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
           </div>
